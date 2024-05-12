e6fe25f872fcb2b6648ac5f07f403bcedbe9159e
osvaldosilitonga <osvaldosilitonga@gmail.com>
2024-05-12 11:48:04 +0700

    initial commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("cant load .env file")
+	}
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	port := os.Getenv("PORT")
+	r.Run(fmt.Sprintf(":%s", port))
+}
